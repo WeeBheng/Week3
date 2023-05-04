3f6ff2d4f9ccd89acfb5dc45eb62ab443f300dbf
Koh Wee Bheng <22016788@myrp.edu.sg>
2023-05-04 11:25:50 +0800

    Completed up to section C

--- app/src/main/java/sg/edu/rp/c346/id22016788/week3/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22016788/week3/MainActivity.java
@@ -3,12 +3,35 @@ package sg.edu.rp.c346.id22016788.week3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText etInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the action
+                String stringResponse = etInput.getText().toString();
+                tvDisplay.setText(stringResponse);
+
+            }
+        });
+
     }
 }
